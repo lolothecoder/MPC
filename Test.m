b4ec4697bf5189893670601cc1d319f47475d5d9
Lolo <loicfinette@gmail.com>
2023-12-13 11:58:04 +0100

    Added basic files

---
+++ Test.m
@@ -0,0 +1,8 @@
+Ts = 1/20
+rocket = Rocket(Ts);
+Tf = 2.0; % Simulation end time
+x0 = [deg2rad([2 -2 0, -2 2 0]), 0 0 0, 0 0 0]'; % (w, phi, v, p) Initial state
+u = [deg2rad([0 0]), 80, 0 ]'; % (d1 d2 Pavg Pdiff) Constant input
+[T, X, U] = rocket.simulate(x0, Tf, u); % Simulate unknown, nonlinear model
+rocket.anim_rate = 1.0; % Visualize at 1.0x real−time
+rocket.vis(T, X, U)
